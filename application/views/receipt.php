b75f6929e46abc16aadb163ba1b4daa57bd819a5
CGNY <christian@cgnewyork.com>
2018-11-29 14:34:40 +0800

    SS15 receipt update

--- application/views/receipt.php
+++ application/views/receipt.php
@@ -63,17 +63,39 @@
                                                 <h4 id=""><?php echo !empty($cart->uc_ship_notes) ? $cart->uc_ship_notes : "-"; ?></h4>
                                                 <br>
                                                 <br>
-                                                <h4>
+                                        <table class="table table-striped">
+                                            <thead>
+                                            <tr>
+                                                <th scope="col">Image ID</th>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Qty</th>
+                                                <th scope="col">Size Cost</th>
+                                                <th scope="col">Base Material Cost</th>
+                                                <th scope="col">Total</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
                                                     <?php
                                                     $total = 0;
                                                     foreach($items as $k => $item)
-                                                    {                                                        
-                                                       echo "<p> <img src='". $item->p_url."' style='max-height:200px;max-width:60px;width:auto' /> <br> ".
-                                                               "ID ".$item->p_id." - ".$item->p_price." x Qty ( ".$item->c_qty." ) + Size ( $". $item->ps_price ." ) + $". number_format(MAT_PRICE,2) ." = ".$item->c_final_price."</p> <hr>";
-                                                       $total += $item->c_final_price;
+                                                    {
+                                                        ?>
+                                                        <td> <?php echo $item->p_id; ?> </td>
+                                                        <td> <?php echo "<img src='". $item->p_url."' style='max-height:200px;max-width:60px;width:auto' />"; ?> </td>
+                                                        <td> <?php echo $item->p_price; ?> </td>
+                                                        <td> <?php echo $item->c_qty; ?> </td>
+                                                        <td> <?php echo $item->ps_price; ?> </td>
+                                                        <td> <?php echo number_format(MAT_PRICE,2); ?> </td>
+                                                        <td> <?php echo $item->c_final_price; ?> </td>
+                                                    <?php
+                                                        $total += $item->c_final_price;
                                                     }
                                                     ?>
-                                                </h4>
+
+                                            </tbody>
+                                        </table>
+
                                                 <br>
                                                 <h4>Total Price: <?php echo number_format($total,2); ?></h4>
                                                 <br>
